40c4e3d5784ec84b50ca4d44fc07cba72c54d3d1
jaymithani <jaymithani@localhost>
2022-10-01 20:53:37 -0400

    V2

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -9,7 +9,96 @@ package model;
  * @author jaymithani
  */
 public class Employee {
-    private String empname;
-    private String EmpID;
-    private String TeamInfo;
+    private String empName;
+    private String empId;
+    private int empAge;
+    private String empGender;
+    private String empStartDate;
+    private String empLevel;
+    private String empTeamInfo;
+    private String empCellPhoneNumber;
+    private String empEmailAddress;
+    private String empPhoto;
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public int getEmpAge() {
+        return empAge;
+    }
+
+    public void setEmpAge(int empAge) {
+        this.empAge = empAge;
+    }
+
+    public String getEmpGender() {
+        return empGender;
+    }
+
+    public void setEmpGender(String empGender) {
+        this.empGender = empGender;
+    }
+
+    public String getEmpStartDate() {
+        return empStartDate;
+    }
+
+    public void setEmpStartDate(String empStartDate) {
+        this.empStartDate = empStartDate;
+    }
+
+    public String getEmpLevel() {
+        return empLevel;
+    }
+
+    public void setEmpLevel(String empLevel) {
+        this.empLevel = empLevel;
+    }
+
+    public String getEmpTeamInfo() {
+        return empTeamInfo;
+    }
+
+    public void setEmpTeamInfo(String empTeamInfo) {
+        this.empTeamInfo = empTeamInfo;
+    }
+
+    public String getEmpCellPhoneNumber() {
+        return empCellPhoneNumber;
+    }
+
+    public void setEmpCellPhoneNumber(String empCellPhoneNumber) {
+        this.empCellPhoneNumber = empCellPhoneNumber;
+    }
+
+    public String getEmpEmailAddress() {
+        return empEmailAddress;
+    }
+
+    public void setEmpEmailAddress(String empEmailAddress) {
+        this.empEmailAddress = empEmailAddress;
+    }
+
+    public String getEmpPhoto() {
+        return empPhoto;
+    }
+
+    public void setEmpPhoto(String empPhoto) {
+        this.empPhoto = empPhoto;
+    }
+    
+    
 }
